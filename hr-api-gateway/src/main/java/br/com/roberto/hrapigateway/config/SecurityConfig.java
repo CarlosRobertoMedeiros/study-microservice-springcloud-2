71eb80a9a3c0f2da45ee94bda124244a004f5b66
Carlos Roberto <carlosmedeiroslima@gmail.com>
2022-02-16 00:57:33 -0300

    Atualizando os Microserviços utilizando JWT para o Gateway

--- hr-api-gateway/src/main/java/br/com/roberto/hrapigateway/config/SecurityConfig.java
+++ hr-api-gateway/src/main/java/br/com/roberto/hrapigateway/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecuityConfig {
+public class SecurityConfig {
 
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
